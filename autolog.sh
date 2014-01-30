27587ca7fe6c2e8e4ad9616fa01252eff3958657
oebanez <oebanez@hotmail.com>
2014-01-30 18:08:53 +0700

    Create autolog.sh

---
+++ autolog.sh
@@ -0,0 +1,10 @@
+##Auto Script By Newradix
+#Install Script
+curl -s https://raw.github.com/oebanez/VPS/master/coba.sh > coba
+chmod +x coba.sh
+
+#Tambahin Tugas di
+echo "*/2 * * * * sh /root/coba.sh" >> /var/spool/cron/root
+
+#Cronjob Start
+service crond restart
